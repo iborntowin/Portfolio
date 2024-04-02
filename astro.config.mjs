0c8584a6ffec3ee62396f13726139eadea90255a
iborntowin <nassim28maaoui@gmail.com>
2024-04-02 13:02:00 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,4 @@
 import { defineConfig } from "astro/config";
 
-export default defineConfig({
-  site: "https://iborntowin.github.io",
-  base: "https://github.com/iborntowin/Portfolio.github.io",
-});
+// https://astro.build/config
+export default defineConfig({});
